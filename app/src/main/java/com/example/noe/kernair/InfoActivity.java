08813c3f87a06e65034968c655284bd3f8f98451
noeRamirez <noe.ramirez1997@email.bakersfieldcollege.edu>
2018-04-13 13:26:05 -0700

    app

--- app/src/main/java/com/example/noe/kernair/InfoActivity.java
+++ app/src/main/java/com/example/noe/kernair/InfoActivity.java
@@ -16,6 +16,7 @@ public class InfoActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /**
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +24,7 @@ public class InfoActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        }); */
     }
+
 }
