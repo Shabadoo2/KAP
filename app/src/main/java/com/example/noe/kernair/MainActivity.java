6ab33a4d86cd185c7bda31251837de2b7868089c
noeRamirez <noe.ramirez1997@email.bakersfieldcollege.edu>
2018-09-24 23:43:24 -0700

    test

--- app/src/main/java/com/example/noe/kernair/MainActivity.java
+++ app/src/main/java/com/example/noe/kernair/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.noe.kernair;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
